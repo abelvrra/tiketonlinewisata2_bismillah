575ad7edd083b28478d7e70e3e5ae961b87014ac
abelvrra <abelvoura@student.uns.ac.id>
2026-04-29 16:29:08 +0700

    up

--- api/logout.php
+++ api/logout.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
 
+// Hapus Cookie dengan set waktu ke -3600 (masa lalu)
+setcookie('id', '', time() - 3600, "/");
+setcookie('key', '', time() - 3600, "/");
+
 header("Location: login.php");
+exit;
 ?>
