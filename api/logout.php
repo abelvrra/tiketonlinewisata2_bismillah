1dd4d5d890a72b479060f0784ef2c3072113f639
abelvrra <abelvoura@student.uns.ac.id>
2026-04-29 19:38:57 +0700

    up

--- api/logout.php
+++ api/logout.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
+
+// Hapus semua cookie yang dibuat saat login
+setcookie('login', '', time() - 3600, "/");
+setcookie('nama',  '', time() - 3600, "/");
+setcookie('role',  '', time() - 3600, "/");
+setcookie('id',    '', time() - 3600, "/");
+setcookie('key',   '', time() - 3600, "/");
+
+// Hapus session juga
+session_unset();
 session_destroy();
 
 header("Location: login.php");
+exit;
 ?>
